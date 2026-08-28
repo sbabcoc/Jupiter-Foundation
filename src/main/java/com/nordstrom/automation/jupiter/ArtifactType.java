291a03a026cbc2a6a0d34caef354b6e98c83c801
Scott Babcock <scoba@hotmail.com>
2026-08-27 21:31:08 -0700

    Add JavaDoc to ArtifactType

--- src/main/java/com/nordstrom/automation/jupiter/ArtifactType.java
+++ src/main/java/com/nordstrom/automation/jupiter/ArtifactType.java
@@ -15,17 +15,48 @@ import com.nordstrom.common.file.PathUtils;
  */
 public abstract class ArtifactType {
 
+    /**
+     * Get the SLF4J {@link Logger} for this artifact type.
+     *
+     * @return logger for this artifact (may be {@code null})
+     */
     public Logger getLogger() {
         return null;
     }
 
+    /**
+     * Determine if artifact capture is available in the specified context.
+     *
+     * @param instance test class instance
+     * @return {@code true} if capture is available; otherwise {@code false}
+     */
     public abstract boolean canGetArtifact(Object instance);
 
+    /**
+     * Capture an artifact from the specified context.
+     *
+     * @param instance test class instance
+     * @param reason impetus for capture request; may be {@code null}
+     * @return byte array containing the captured artifact; if capture fails, an empty array is returned
+     */
     public abstract byte[] getArtifact(Object instance, Throwable reason);
 
+    /**
+     * Get the path at which artifacts of this type should be stored.
+     * <p>
+     * <b>NOTE</b>: The returned path can be either relative or absolute.
+     *
+     * @param instance test class instance
+     * @return artifact storage path
+     */
     public Path getArtifactPath(Object instance) {
         return PathUtils.ReportsDirectory.getPathForObject(instance);
     }
 
+    /**
+     * Get the extension for artifact files of this type.
+     *
+     * @return artifact file extension
+     */
     public abstract String getArtifactExtension();
 }
